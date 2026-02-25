3c0a6b3ba785fc878963d43264d2d46e316db2fb
vladislavkorobeinikov <vladkorobeinikov@gmail.com>
2026-02-26 03:36:20 +0400

    hw lesson singleton & producer

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,38 +1,7 @@
 import time
 import uuid
-from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
 from framework.internal.kafka.producer import Producer
-
-
-def test_failed_registration(account: AccountApi, mail: MailApi) -> None:
-    expected_mail = "string@mail.ru"
-    account.register_user(
-        login="string",
-        email=expected_mail,
-        password="string"
-    )
-    for _ in range(10):
-        response = mail.find_message(expected_mail)
-        if response.json()['total'] > 0:
-            break
-        else:
-            raise AssertionError("Email is not found")
-
-def test_success_registration(account: AccountApi, mail: MailApi) -> None:
-    base = uuid.uuid4().hex
-    expected_mail = f"{base}@mail.ru"
-    account.register_user(
-        login=base,
-        email=expected_mail,
-        password="string"
-    )
-    for _ in range(10):
-        response = mail.find_message(expected_mail)
-        if response.json()['total'] > 0:
-            break
-        else:
-            raise AssertionError("Email is not found")
 
 
 def test_success_registration_with_kafka_producer(mail: MailApi, kafka_producer: Producer) -> None:
@@ -41,7 +10,7 @@ def test_success_registration_with_kafka_producer(mail: MailApi, kafka_producer:
     message = {
         "login": login,
         "email": f"{login}@mail.ru",
-        "password": "123"
+        "password": "1jksdnfjsadnfsa23"
     }
     kafka_producer.send('register-events', message)
     for _ in range(10):
