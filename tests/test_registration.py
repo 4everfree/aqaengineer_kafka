1f9793e78d51b0f92f4373b54862333d2f173684
4everfree <vladkorobeinikov@gmail.com>
2026-03-17 01:42:57 +0400

    hw rabbit lesson 1

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,8 @@
+import json
 import time
+import uuid
+
+import pika
 
 from framework.helpers.kafka.consumers.register_events import RegisterEventsSubscriber
 
@@ -36,3 +40,39 @@ def test_success_registration_with_kafka_consumer_observer(
             break
     else:
         raise AssertionError("Email not found")
+
+def test_rabbit():
+    connection = pika.BlockingConnection(
+        pika.URLParameters('amqp://guest:guest@185.185.143.231:5672')
+    )
+    channel = connection.channel()
+    address = f"{uuid.uuid4().hex}@mail.ru"
+    message = {
+        "address": address,
+        "subject": "test",
+        "body": "to qa or not to qa",
+    }
+
+    message = json.dumps(message).encode('utf-8')
+
+    try:
+        exchange = channel.exchange_declare(
+            exchange_type='topic',
+            exchange='dm.mail.sending',
+            durable=True
+        )
+
+        properties = pika.BasicProperties(
+            content_type='application/json',
+            correlation_id=str(uuid.uuid4()),
+        )
+
+        channel.basic_publish(
+            exchange='dm.mail.sending',
+            routing_key='',
+            body=message,
+            properties=properties,
+        )
+    finally:
+        channel.close()
+        connection.close()
